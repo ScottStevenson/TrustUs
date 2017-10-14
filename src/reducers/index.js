7b2afc93bd29566b7f8dffe68e4421012f6858e0
Jeff Lau <jeff-lau@live.com>
2017-10-14 09:10:36 -0400

    Added redux/react router and default setup

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+
+function reducer(state = {}, action){
+  return state
+}
+
+const reducers = combineReducers( {
+    reducer
+} );
+
+export default reducers
