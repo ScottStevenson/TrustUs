40eee6f467c88b5e9550b3e143755d49378e3f96
Jeff Lau <jeff-lau@live.com>
2017-10-15 01:28:14 -0400

    dump values into trust summary

--- src/components/TrustSummary.js
+++ src/components/TrustSummary.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const TrustSummary = ({ contractArguments}) => {
+  console.log(contractArguments)
+  let triggerType = contractArguments.trigger.value
+  let beneficaries = contractArguments.beneficaries.value.map(bene =>
+    <div>
+      {bene}
+    </div>)
+
+  let trustType = contractArguments.typeOfTrust.value
   return (
     <div>
-      {contractArguments}
-      Trust Summary
+      <h2>Trust Summary</h2>
+      <div>Type of Trust: {trustType}</div>
+      <div>{beneficaries}</div>
+      <div>Trigger: {triggerType}</div>
     </div>
   )
 }
